9e5f233665ef531a2a52d7358f33139e86c42934
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2022-01-04 12:04:05 -0800

    added logic to the search bar

--- react-bartender-v2/src/components/SearchBar/SearchBar.js
+++ react-bartender-v2/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import SearchIcon from '@mui/icons-material/Search';
 const SearchBar = () => {
+    //created a state for searched drink name 
+    const [drink, setDrink] = useState(''); //initially it is an empty string 
+    //add a listener to the text input 
+    const handleDrinkChange = (e) => {
+        setDrink(e.target.value);
+    }
+    //create a function for the form submit 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //call the api to search drinks
+        //empty out text string
+        //set home state to false 
+
+    }
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <Box sx={{
                 display: 'flex',
                 alignItems: 'center',
@@ -13,8 +27,13 @@ const SearchBar = () => {
                 flexDirection: 'row',
                 padding: '20px',
             }}>
-                <TextField id="outlined-basic" label="Enter Cocktail" variant="outlined" />
-                <Button variant="text"><SearchIcon/></Button>
+                <TextField id="outlined-basic" label="Enter Cocktail" variant="outlined" 
+                value={drink}
+                onChange={handleDrinkChange}
+                />
+                <Button variant="text"
+                type='submit'
+                ><SearchIcon/></Button>
             </Box>
 
         </form>
